74c7fc543e985aa7a7ed68ebfaa4d2008172b684
Anderson Coelho <anderson.coelho@indt.org.br>
2025-01-23 11:46:38 -0400

    add method converter on route

--- converter.py
+++ converter.py
@@ -1,21 +1,25 @@
 from os import getenv
 import requests
 from fastapi import HTTPException
+from dotenv import load_dotenv
 
-ALPHAVANTAGE_APIKEY = getenv('ALPHAVANTAGE_APIKEY')
+load_dotenv()
+
+API_KEY = getenv("ALPHAVANTAGE_APIKEY")
 
 def sync_converter(from_currency: str, to_currency: str, value: float):
-    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={ALPHAVANTAGE_APIKEY}'
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}"
 
     try:
         response = requests.get(url=url)
     except Exception as error:
-        raise HTTPException(status_code=400, detail=error)
+        raise HTTPException(status_code=424, detail=error)
 
     data = response.json()
 
     if "Realtime Currency Exchange Rate" not in data:
-        raise HTTPException(status_code=400, detail='Realtime Currency Exchange Rate is missing.')
+        print(f"API Response: {data}")
+        raise HTTPException(status_code=422, detail='Realtime Currency Exchange Rate is missing.')
 
     exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
 
