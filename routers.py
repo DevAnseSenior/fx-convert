46fb4e72352e45d59bd1e52be1f9aa1c3ae659e5
Anderson Coelho <anderson.coelho@indt.org.br>
2025-01-22 21:23:10 -0400

    create sync convert method

--- routers.py
+++ routers.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get('/converter/{from_currency}')
-def converter(from_currency: str, to_currencies: str, price: float):
+def converter(from_currency: str, to_currencies: str, value: float):
     return "Converter works!!!"
